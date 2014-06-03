4b345f5722854a0a97b03add87b6d2f1a5567ad8
Kai Di Chen <kdchen115@gmail.com>
2014-06-03 12:59:20 -0700

    added variance heatmap by utility

--- src/CriteriaStatistics.java
+++ src/CriteriaStatistics.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -166,7 +167,109 @@ public class CriteriaStatistics {
     }
     
     public static HashMap<String, Integer> rankVarianceUtility(ArrayList<IndividualAttributeMaps> listOfAttributeMaps) {
-        // TODO
-        return null;
+        // for each criteria, for each x-coordinate (if discrete, name; if continuous, string of x-coord)
+        HashMap<String, HashMap<String, Double>> attributeMeans = new HashMap<String, HashMap<String, Double>>();
+        HashMap<String, HashMap<String, ArrayList<Double>>> attributeValues = new HashMap<String, HashMap<String, ArrayList<Double>>>();
+        
+        // for each user
+        for (IndividualAttributeMaps iam : listOfAttributeMaps) {
+            
+            // for each criteria
+            for (Map.Entry<String, AttributeDomain> attrDom : iam.attributeDomainMap.entrySet()) {
+                
+                int xCount;
+                AttributeDomain dom = attrDom.getValue();
+                double[] yval = dom.getWeights();
+                String[] xStr = dom.getElements();
+                double[] xNum = dom.getKnots();
+                
+                // for each coordinate point
+                for (int i = 0; i < yval.length; i++) {
+                    DecimalFormat df = new DecimalFormat("#.############"); // in case of roundoff error
+                    String xkey = (xStr != null ? xStr[i] : df.format(xNum[i]));
+                    
+                    // get criteria 
+                    String key = attrDom.getKey();
+                    HashMap<String, Double> means = attributeMeans.get(key);
+                    HashMap<String, ArrayList<Double>> valuesMap = attributeValues.get(key);
+                    if (means == null) {
+                        means = new HashMap<String, Double>();
+                        attributeMeans.put(key, means);
+                    } 
+                    if (valuesMap == null) {
+                        valuesMap = new HashMap<String, ArrayList<Double>>();
+                        attributeValues.put(key, valuesMap);
+                    }
+                    
+                    // get coordinate
+                    ArrayList<Double> values = valuesMap.get(xkey);
+                    
+                    // get the y-coordinate
+                    double val = yval[i];
+                    
+                    if (values == null) {
+                        values = new ArrayList<Double>();
+                        valuesMap.put(xkey, values);
+                    }
+                    values.add(val);
+                    
+                    Double mean = means.get(xkey);
+                    if (mean == null) {
+                        means.put(xkey, val);
+                    } else {
+                        means.put(xkey, mean + val);
+                    }
+                }
+            }
+        }
+        
+        double varMin = Double.MAX_VALUE;
+        double varMax = Double.MIN_VALUE;
+        HashMap<String, Double> vars = new HashMap<String, Double>();
+        // for each criteria
+        for (Map.Entry<String, HashMap<String, ArrayList<Double>>> valuesMap : attributeValues.entrySet()) {
+            // a variance is calculated for each criteria, each x-coordinate
+            // will take the median variance to get variance for each criteria
+            ArrayList<Double> varMed = new ArrayList<Double>(); 
+            
+            // for x-coord
+            for (Map.Entry<String, ArrayList<Double>> values : valuesMap.getValue().entrySet()) {
+                
+                double var = 0;
+                ArrayList<Double> vals = values.getValue();
+                Double mean = attributeMeans.get(valuesMap.getKey()).get(values.getKey());
+                mean = mean / vals.size();
+                        
+                for (Double v : vals) {
+                    var += Math.pow(v-mean, 2);
+                }
+                var = var / (vals.size()-1);
+                varMed.add(var);
+            }
+            
+            int idx = varMed.size()/2;
+            double var = varMed.get(idx);
+            if (varMed.size() % 2 == 0) {
+                var += varMed.get(idx+1);
+                var /= 2;
+            }
+            
+            if (var < varMin)
+                varMin = var;
+            if (var > varMax)
+                varMax = var;
+            
+            vars.put(valuesMap.getKey(), var);
+        }
+        
+        if (varMin >= varMax) return null;
+        
+        HashMap<String, Integer> rankVarScore = new HashMap<String, Integer>();
+        for (Map.Entry<String, Double> var : vars.entrySet()) {
+            int rank = (int) Math.round((var.getValue() - varMin) / (varMax - varMin) * 8);
+            rankVarScore.put(var.getKey(), rank);
+        }
+        
+        return rankVarScore;
     }
 }
